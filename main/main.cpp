771e443cbae5b06c7315c24898168b6e878b3361
Dylan Luo <dylan.luo@m5stack.com>
2025-05-20 14:50:33 +0800

    fix

--- main/main.cpp
+++ main/main.cpp
@@ -21,39 +21,79 @@
 #include <M5Unified.h>
 
 #define LED_STRIP_BLINK_GPIO  21
-#define LED_STRIP_LED_NUMBERS 1
+#define LED_STRIP_LED_NUMBERS 3
 #define LED_STRIP_RMT_RES_HZ  (10 * 1000 * 1000)
 
 static const char *TAG = "LED_STRIP";
 
 led_strip_handle_t led_strip;
 
-static uint8_t rgb_state[3] = {255, 255, 255};
+static uint8_t rgb_state[3][3] = {{255, 0, 0}, {0, 255, 0}, {0, 0, 255}};
 
 void update_display() {
     M5.Display.clear();
     M5.Display.setCursor(10, 10);
     M5.Display.print("Unit RGB TEST");
     M5.Display.setCursor(10, 50);
-    M5.Display.print("R:");
-    M5.Display.print(rgb_state[0] == 255 ? "ON" : "OFF");
+    M5.Display.print("L: ");
+    M5.Display.print(rgb_state[0][0]);
+    M5.Display.print(", ");
+    M5.Display.print(rgb_state[0][1]);
+    M5.Display.print(", ");
+    M5.Display.print(rgb_state[0][2]);
+
     M5.Display.setCursor(10, 100);
-    M5.Display.print("G:");
-    M5.Display.print(rgb_state[1] == 255 ? "ON" : "OFF");
+    M5.Display.print("M: ");
+    M5.Display.print(rgb_state[1][0]);
+    M5.Display.print(", ");
+    M5.Display.print(rgb_state[1][1]);
+    M5.Display.print(", ");
+    M5.Display.print(rgb_state[1][2]);
+
     M5.Display.setCursor(10, 150);
-    M5.Display.print("B:");
-    M5.Display.print(rgb_state[2] == 255 ? "ON" : "OFF");
+    M5.Display.print("R: ");
+    M5.Display.print(rgb_state[2][0]);
+    M5.Display.print(", ");
+    M5.Display.print(rgb_state[2][1]);
+    M5.Display.print(", ");
+    M5.Display.print(rgb_state[2][2]);
+}
+
+void move_rgb(int index) 
+{
+    if (rgb_state[index][0] == 255) 
+    {
+        rgb_state[index][0] = 0;
+        rgb_state[index][1] = 255;
+        rgb_state[index][2] = 0;
+    } 
+    else 
+    if (rgb_state[index][1] == 255)
+    {
+        rgb_state[index][0] = 0;
+        rgb_state[index][1] = 0;
+        rgb_state[index][2] = 255;
+    }
+    else 
+    if (rgb_state[index][2] == 255)
+    {
+        rgb_state[index][0] = 255;
+        rgb_state[index][1] = 0;
+        rgb_state[index][2] = 0;
+    }
 }
 
 extern "C" void app_main(void)
 {
     M5.begin();
+    M5.Ex_I2C.release();
     M5.Speaker.tone(2000, 100, 0, false);
     M5.Speaker.tone(1000, 100, 0, false);
     M5.Display.fillScreen(0x222222);
     M5.Display.setTextColor(TFT_WHITE);
     M5.Display.setTextSize(3);
     update_display();
+
     // LED strip general initialization, according to your led board design
     led_strip_config_t strip_config = {
         .strip_gpio_num = LED_STRIP_BLINK_GPIO,   // The GPIO that connected to the LED strip's data line
@@ -79,7 +119,9 @@ extern "C" void app_main(void)
 
     ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
     ESP_LOGI(TAG, "Created LED strip object with RMT backend");
-    led_strip_set_pixel(led_strip, 0, 255, 255, 255);
+    led_strip_set_pixel(led_strip, 0, rgb_state[0][0], rgb_state[0][1], rgb_state[0][2]);
+    led_strip_set_pixel(led_strip, 1, rgb_state[1][0], rgb_state[1][1], rgb_state[1][2]);
+    led_strip_set_pixel(led_strip, 2, rgb_state[2][0], rgb_state[2][1], rgb_state[2][2]);
     led_strip_refresh(led_strip);
     
     while (1) 
@@ -89,41 +131,24 @@ extern "C" void app_main(void)
         if (M5.BtnA.wasClicked()) {
             M5.Speaker.tone(2000, 200);
             M5_LOGI("Btn Left Clicked");
-            rgb_state[0] = (rgb_state[0] == 255) ? 0 : 255;
+            move_rgb(0);
             update_display();
         }
         if (M5.BtnB.wasClicked()) {
             M5.Speaker.tone(2000, 200);
             M5_LOGI("Btn Middle Clicked");
-            rgb_state[1] = (rgb_state[1] == 255) ? 0 : 255;
+            move_rgb(1);
             update_display();
         }
         if (M5.BtnC.wasClicked()) {
             M5.Speaker.tone(2000, 200);
             M5_LOGI("Btn Right Clicked");
-            rgb_state[2] = (rgb_state[2] == 255) ? 0 : 255;
+            move_rgb(2);
             update_display();
         }
-        
-        static uint8_t breath_val = 0;
-        static bool breath_increasing = true;
-        
-        // 计算呼吸效果的亮度值
-        if (breath_increasing) {
-            breath_val = (breath_val >= 255) ? 255 : breath_val + 5;
-            if (breath_val >= 255) breath_increasing = false;
-        } else {
-            breath_val = (breath_val <= 0) ? 0 : breath_val - 5;
-            if (breath_val <= 0) breath_increasing = true;
-        }
-        
-        // 应用呼吸效果，如果RGB值为0则不应用呼吸效果
-        uint32_t r = rgb_state[0] > 0 ? (rgb_state[0] * breath_val) / 255 : 0;
-        uint32_t g = rgb_state[1] > 0 ? (rgb_state[1] * breath_val) / 255 : 0;
-        uint32_t b = rgb_state[2] > 0 ? (rgb_state[2] * breath_val) / 255 : 0;
-        
-        led_strip_set_pixel(led_strip, 0, r, g, b);
+        led_strip_set_pixel(led_strip, 0, rgb_state[0][0], rgb_state[0][1], rgb_state[0][2]);
+        led_strip_set_pixel(led_strip, 1, rgb_state[1][0], rgb_state[1][1], rgb_state[1][2]);
+        led_strip_set_pixel(led_strip, 2, rgb_state[2][0], rgb_state[2][1], rgb_state[2][2]);
         led_strip_refresh(led_strip);
-
     }
 }
